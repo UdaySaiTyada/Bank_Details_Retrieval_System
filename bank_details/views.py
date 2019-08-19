2c43f56c5a27b1558a550aaadfdc401cd8271240
Uday Sai Tyada <udaysai2551998@gmail.com>
2019-08-20 01:24:49 +0530

    JWT Authentication added

--- bank_details/views.py
+++ bank_details/views.py
@@ -1,22 +1,46 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 import json
 
 from bank_details.models import Banks
-# Create your views here.
-@csrf_exempt
-def get_bank_details(request):
-    print(request.body)
 
-    received_json_data = json.loads(request.body)
-    input_Ifsc = str(received_json_data['ifsc'])
-    bank_data = Banks.objects.get(ifsc=input_Ifsc)
-    return JsonResponse({'ifsc': bank_data.ifsc,
-                         'bank_id' : bank_data.bank_id,
-                         'branch': bank_data.branch,
-                         'address' : bank_data.address,
-                         'city' :  bank_data.city,
-                         'district' : bank_data.district,
-                         'state' : bank_data.state,
-                         'bank_name' : bank_data.bank_name})
+# Code with JWT Authentication
+class Bank_details(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        print(request.body)
+        received_json_data = json.loads(request.body)
+        input_Ifsc = str(received_json_data['ifsc'])
+        bank_data = Banks.objects.get(ifsc=input_Ifsc)
+        return JsonResponse({'ifsc': bank_data.ifsc,
+                             'bank_id': bank_data.bank_id,
+                             'branch': bank_data.branch,
+                             'address': bank_data.address,
+                             'city': bank_data.city,
+                             'district': bank_data.district,
+                             'state': bank_data.state,
+                             'bank_name': bank_data.bank_name})
+
+
+# Code with out JWT Authentication
+
+# @csrf_exempt
+# def get_bank_details(request):
+#     print(request.body)
+#
+#     received_json_data = json.loads(request.body)
+#     input_Ifsc = str(received_json_data['ifsc'])
+#     bank_data = Banks.objects.get(ifsc=input_Ifsc)
+#     return JsonResponse({'ifsc': bank_data.ifsc,
+#                          'bank_id' : bank_data.bank_id,
+#                          'branch': bank_data.branch,
+#                          'address' : bank_data.address,
+#                          'city' :  bank_data.city,
+#                          'district' : bank_data.district,
+#                          'state' : bank_data.state,
+#                          'bank_name' : bank_data.bank_name})
--- fyle_task/urls.py
+++ fyle_task/urls.py
@@ -20,8 +20,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bank_details/', views.get_bank_details),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
-
+    path('bank_details/', views.Bank_details.as_view()),
 ]
